c6f28dab76d755b06ad874cbb01a2752e5c760b5
tugra103 <tugrabulbul2020@gmail.com>
2026-05-13 08:23:20 +0300

    Make start property optional in CalendarEvent interface

--- types/event.ts
+++ types/event.ts
@@ -1,9 +1,17 @@
+// types/event.ts
 export interface CalendarEvent {
   id: string
   title: string
-  start: string
+  start?: string
   end?: string
   allDay?: boolean
   color?: string
   description?: string
+  rrule?: {
+    freq: string
+    byweekday?: string[]
+    dtstart?: string
+    until?: string
+  }
+  duration?: string
 }
